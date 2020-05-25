1504388df4c55dc9829d1abec8906cd86b1f0a6b
Burak Kepti <bkepti@gmail.com>
2020-05-25 15:28:40 +0300

    Update App.xaml.cs

--- App123/App.xaml.cs
+++ App123/App.xaml.cs
@@ -6,8 +6,17 @@ namespace App123
 {
     public partial class App : Application
     {
+        public static string DatabasePath;
         public App()
         {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+        
+        public App(string databasePath)
+        {
+            DatabasePath=databasePath;
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
